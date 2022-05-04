a4a7650c72a4cb092bc4915f97dec8665a7f2206
scottly426 <lyscott123@gmail.com>
2022-05-04 12:55:42 -0700

    Adding initial files for fashion studio

---
+++ src/sections/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import CoverVideo from "../components/CoverVideo";
+const Section = styled.section`
+  position: relative;
+  min-height: 100vh;
+  overflow: hidden;
+`;
+
+export default function Home() {
+  return (
+    <Section>
+      <CoverVideo />
+      <h1>Logo</h1>
+      <h1>NavBar</h1>
+    </Section>
+  );
+}
